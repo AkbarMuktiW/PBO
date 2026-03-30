1986dd50857303b0fa59cfbb44da6c8c60a105c8
unknown <akbarmuktiwibowo23@gmail.com>
2026-03-30 17:20:12 +0700

    Membuat main RestoNyawiti

--- RestoNyawiti/FullCodeMain/src/MenuException.java
+++
@@ -1,5 +0,0 @@
-public class MenuException extends Exception {
-    public MenuException(String message) {
-        super(message);
-    }
-}
--- RestoNyawiti/Transaksi/src/ICalculatable.java
+++
@@ -1,8 +0,0 @@
-public interface ICalculatable {
-    /* ATRIBUT */
-    double PPN = 0.12;
-    
-    /* METHOD */
-    double hitungPajak(double total);
-    double hitungDiskon(double total);
-}
