4f16f6d935a27ef15a442fa008d60b7407fd8204
unknown <akbarmuktiwibowo23@gmail.com>
2026-03-31 13:23:08 +0700

    Update RestoNyawiti

--- RestoNyawiti/src/ICalculatable.java
+++ RestoNyawiti/src/ICalculatable.java
@@ -1,3 +1,11 @@
+/*  Anggota Kelompok:
+*   Akbar Mukti Wibowo				24060124130063
+*   Farras Hilmy Zaidan 			24060124120003
+*   Agil Yudis Wibawa 				24060124120045
+*   Aufaarel Nabiil Aryadh Mecca 	24060124140206
+*   Fakhri Ali Azadi				24060123140128
+*/
+
 public interface ICalculatable {
     /* ATRIBUT */
     double PPN = 0.12;
--- RestoNyawiti/src/MenuException.java
+++ RestoNyawiti/src/MenuException.java
@@ -1,3 +1,11 @@
+/*  Anggota Kelompok:
+*   Akbar Mukti Wibowo				24060124130063
+*   Farras Hilmy Zaidan 			24060124120003
+*   Agil Yudis Wibawa 				24060124120045
+*   Aufaarel Nabiil Aryadh Mecca 	24060124140206
+*   Fakhri Ali Azadi				24060123140128
+*/
+
 public class MenuException extends Exception {
     public MenuException(String message) {
         super(message);
